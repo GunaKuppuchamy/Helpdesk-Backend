6644ae725eb9872e130e2390ec9f5e6032b95016
thanzia31 <thanzi2004@gmail.com>
2025-06-18 18:43:25 +0530

    code clean commit

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+
 const Ticket = require('../Models/tickets');
 const user=require('../Models/employees');
 const nodemailer = require('../nodemailer-config')
