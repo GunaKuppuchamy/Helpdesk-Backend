46982cb56c8874d2a7711958c2d3eaab2f178b09
Sree-Suvetha <sreesuvethag@gmail.com>
2025-06-26 16:11:49 +0530

    health controller changes

--- Controllers/healthController.js
+++ Controllers/healthController.js
@@ -3,17 +3,34 @@ const mongoose = require('mongoose');
 exports.healthCheck = (req, res) => {
   const dbState = mongoose.connection.readyState;
 
+  // once the dbstate is fetched, we can map it to a string
   const dbStatus = {
     0: 'disconnected',
     1: 'connected',
     2: 'connecting',
     3: 'disconnecting'
-  }[dbState];
+  }[dbState]; //defining a set of key value pairs and then accessing the value using the dbState as key
+
+
+  // process is a global object in Node.js that provides information about the current Node.js process. 
+ const memory = process.memoryUsage();
+ const cpu = process.cpuUsage();
+ const uptime = process.uptime();
+
 
   const status = {
     status: dbState === 1 ? 'UP' : 'DOWN',
     db: dbStatus,
-    timestamp: Date.now()
+    timestamp: Date.now(),
+    uptime: `${Math.floor(uptime)} seconds`,
+    memory: {
+      rss: memory.rss, //resident set size- (Its the total memory used by node.js process in RAM)
+      heapUsed: memory.heapUsed //memory currently used by your app's variables and objects
+    },
+    cpu: {
+      user: cpu.user, // micro sec spent for running js code (user CPU time)
+      system: cpu.system // micro sec spent for system/kernel operations (system CPU time)
+    }
   };
 
   const code = dbState === 1 ? 200 : 503;
